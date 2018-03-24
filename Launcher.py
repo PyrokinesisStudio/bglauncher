0e13338986652765447e1592efdeb3f65230d769
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2018-03-24 12:14:17 -0300

    Updated Launcher.py

---
+++ Launcher.py
@@ -0,0 +1,49 @@
+import pathlib2
+import subprocess
+from pprint import pprint, pformat
+from ast import literal_eval as litev
+
+Config = {}
+sizes_window = ( (640, 480), (800, 600), (1024, 768), (1280, 720), (1366, 768), (1920, 1080) )
+sizes_fullscreen = ( (0, 0), (640, 480), (800, 600), (1024, 768), (1280, 720), (1366, 768), (1920, 1080) )
+stereo_modes = ( "nostereo", "anaglyph", "sidebyside", "syncdoubling", "3dtvtopbottom", "interlace", "vinterlace", "hwpageflip" )
+bit_depths = ( 16, 24, 32 )
+refresh_rates = ( 30, 45, 60, 75, 90, 120 )
+
+try:
+	with open('Settings.cfg', 'r') as openedfile:
+		
+		Config = litev( openedfile.read() )
+		pprint( 'Settings loaded from ' + openedfile.name )
+		
+except:
+	pprint( 'Couldnt open Settings.cfg, using defaults' )
+	Config = { 'videomode' : 0, 'stereomode' : 0, 'nomipmap' : 0, 'fixedtime' : 0, 'sizewindow' : 2, 'sizefullscreen' : 0, 'bitdepth' : 2, 'refreshrate' : 2, 'pathplayer' : 'engine/blenderplayer.exe', 'pathfile' : 'data/main.blend' }
+	
+if True:
+		
+	pathplayer = pathlib2.Path( Config['pathplayer'] ).resolve()
+	pathfile = pathlib2.Path( Config['pathfile'] ).resolve()
+	
+	command = ''
+	
+	if pathplayer.exists() and pathfile.exists():
+		
+		command += '"' + str(pathplayer) + '" '
+		
+		if Config['videomode'] == 0:
+			command += '-w '+ str(sizes_window[Config['sizewindow']][0]) + ' ' + str(sizes_window[Config['sizewindow']][1]) + ' 0 0 '
+		
+		if Config['videomode'] == 1:
+			command += '-f ' + str(sizes_fullscreen[Config['sizefullscreen']][0]) + ' ' + str(sizes_fullscreen[Config['sizefullscreen']][1]) + ' ' + str(bit_depths[Config['bitdepth']]) + ' ' + str(refresh_rates[Config['refreshrate']]) + ' '
+			
+		command += '-s ' + str(stereo_modes[Config['stereomode']]) + ' '
+		
+		command += '-g fixedtime=' + str(Config['fixedtime']) + ' '
+		
+		command += '"' + str(pathfile) + '"'
+		
+		pprint('Running game under the following command: ')
+		pprint(command)
+		
+		subprocess.Popen(command)
